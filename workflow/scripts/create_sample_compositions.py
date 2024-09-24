02161aa0838924541a24731934bab14aa741bcbb
huzuner <hamdiyeuzuner@gmail.com>
2024-09-24 13:50:03 +0000

    pandemics simulation: normalize fractions after sampling

--- workflow/scripts/create_sample_compositions.py
+++ workflow/scripts/create_sample_compositions.py
@@ -23,12 +23,14 @@ with open(snakemake.log[0], "w") as f:
     #first sample some timepoints 
     sampled_values = sample([list(d.values()) for d in list_of_distributions],n_of_samples)
 
+    #normalization function to normalize fractions in the final composition
+    def normalize_dict_values(data):
+        total = sum(data.values())
+        return {key: value / total for key, value in data.items()}
+    
     #create separate dataframes for each composition and write to csv
     #to do that, simulate combinations of 3 lineages sampled randomly propotional to case count abundances.
     
-    #set random seed for numpy 
-    np.random.seed(42)
-
     sample_name_counter = 1
     for i,d in enumerate(list_of_distributions):
         for s in sampled_values:
@@ -37,15 +39,24 @@ with open(snakemake.log[0], "w") as f:
                 sample_dict_list = []
                 print(list(d.keys()))
                 print(s)
+                #set random seed for numpy 
+                np.random.seed(42)
+
                 sampled_lineages = np.random.choice(list(d.keys()), p=s, size=3, replace=False)
                 print("sampled_lineages:",sampled_lineages)
-                for sampled_lineage in sampled_lineages:
-                    if sampled_lineage in d:
-                        sample_dict = {}
-                        sample_dict['lineage'] = str(sampled_lineage.split(" ")[0])
-                        sample_dict['fraction'] = d[sampled_lineage]
-                        sample_dict['sample'] = "SimulatedSample" + str(sample_name_counter)
-                        sample_dict_list.append(sample_dict)
+                
+                #create sub dictionary with sampled lineages and normalize fractions
+                subset_dict = {key: d[key] for key in sampled_lineages if key in d}
+                normalized_subset_dict=normalize_dict_values(subset_dict)
+                print("normalized_subset_dict:",normalized_subset_dict)
+
+                for l in normalized_subset_dict:
+                    sample_dict = {}
+                    sample_dict['lineage'] = str(l.split(" ")[0])
+                    sample_dict['fraction'] = normalized_subset_dict[l]
+                    sample_dict['sample'] = "SimulatedSample" + str(sample_name_counter)
+                    sample_dict_list.append(sample_dict)
+                        
                 sample_df = pd.DataFrame(sample_dict_list)
                 sample_df.to_csv(snakemake.output.synthetic_samples[sample_name_counter-1], index=False, doublequote=False)
 
