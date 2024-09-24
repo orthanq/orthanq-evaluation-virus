1cb76b1984d2cbeb2582961da0af6820de0ea228
huzuner <hamdiyeuzuner@gmail.com>
2024-09-24 12:25:21 +0000

    pandemics simulation: simulate combinations of 2-3 lineages sampled randomly propotional to case count abundances.

--- workflow/scripts/create_sample_compositions.py
+++ workflow/scripts/create_sample_compositions.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from pathlib import Path
 from random import sample 
+import numpy as np
 import random
 
 with open(snakemake.log[0], "w") as f:
@@ -17,26 +18,34 @@ with open(snakemake.log[0], "w") as f:
     #sample N sets
     list_of_distributions = []
     list_of_distributions = [eval(row['clades_frequencies']) for _, row in df.iterrows()]
-
     random.seed(1313)
 
-    sampled_values = sample([d.values() for d in list_of_distributions],n_of_samples)
+    #first sample some timepoints 
+    sampled_values = sample([list(d.values()) for d in list_of_distributions],n_of_samples)
 
     #create separate dataframes for each composition and write to csv
+    #to do that, simulate combinations of 3 lineages sampled randomly propotional to case count abundances.
+    
+    #set random seed for numpy 
+    np.random.seed(42)
+
     sample_name_counter = 1
     for i,d in enumerate(list_of_distributions):
         for s in sampled_values:
             #when finding the index for the sample, create a csv for each synthetic sample
             if sorted(d.values()) == sorted(s):
                 sample_dict_list = []
-                for key in d:
-                    sample_dict = {}
-                    # print(str(key))
-                    # print(str(key).split(" ")[0])
-                    sample_dict['lineage'] = str(key.split(" ")[0])
-                    sample_dict['fraction'] = d[key]
-                    sample_dict['sample'] = "SimulatedSample" + str(sample_name_counter)
-                    sample_dict_list.append(sample_dict)
+                print(list(d.keys()))
+                print(s)
+                sampled_lineages = np.random.choice(list(d.keys()), p=s, size=3, replace=False)
+                print("sampled_lineages:",sampled_lineages)
+                for sampled_lineage in sampled_lineages:
+                    if sampled_lineage in d:
+                        sample_dict = {}
+                        sample_dict['lineage'] = str(sampled_lineage.split(" ")[0])
+                        sample_dict['fraction'] = d[sampled_lineage]
+                        sample_dict['sample'] = "SimulatedSample" + str(sample_name_counter)
+                        sample_dict_list.append(sample_dict)
                 sample_df = pd.DataFrame(sample_dict_list)
                 sample_df.to_csv(snakemake.output.synthetic_samples[sample_name_counter-1], index=False, doublequote=False)
 
